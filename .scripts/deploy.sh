b48abc0f1c2862ea71686ffa7350c6583cc7696d
adamhaley <adamhaley@gmail.com>
2026-01-30 12:23:57 -0800

    building nocache, should be great for deploy times

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,6 +4,7 @@ set -e
 echo "Deploying .."
 
 git pull origin master
-docker compose -f ../n8n-docker-caddy/docker-compose.yml up -d --build megyk-dashboard
+docker compose -f ../n8n-docker-caddy/docker-compose.yml build --no-cache megyk-dashboard
+docker compose -f ../n8n-docker-caddy/docker-compose.yml up -d megyk-dashboard
 
 echo "Deploy complete!"
